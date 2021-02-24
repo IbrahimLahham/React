3811c1fbfcf466ac87934b63a9c38039921c3513
Jbareen Mohamad <jbareen.jeme@gmail.com>
2021-02-24 19:59:52 +0200

    fix

--- client/src/pages/parliamentaryTool/parliamentaryTool.js
+++ client/src/pages/parliamentaryTool/parliamentaryTool.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD:react/src/pages/parliamentaryTool/parliamentaryTool.js
-
 import React, { useState } from 'react';
 
 import './parliamentaryTool.css'
@@ -18,24 +16,3 @@ const parliamentaryTool = () => {
 }
 
 export default parliamentaryTool
-=======
-
-import React, { useState } from 'react';
-
-import './parliamentaryTool.css'
-
-
-const parliamentaryTool = () => {
-
-  // const [book, setbook] = useState([]);
-  
-
-    return (
-        <div>
-            <h1>parliamentaryToolsPage</h1>
-        </div>
-    )
-}
-
-export default FormCreate
->>>>>>> main:client/src/pages/parliamentaryTool/parliamentaryTool.js
