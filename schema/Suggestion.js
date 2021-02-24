60cc7a14f40ea26df0659b54e7ad74a55a9e382c
Ali Khutaba <khutaba.ali@gmail.com>
2021-02-24 20:26:42 +0200

    add schema db

---
+++ schema/Suggestion.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const User = require("./user");
+const Tool = require("./Tool");
+
+
+const SuggestionSchema = new mongoose.Schema({
+    subject: String,
+    date: { type: Date, default: Date.now },
+    description: String,
+    status: [SuggestionStatus],
+    knessetMembers: [User],
+    toolType: Tool,
+    submittedBy: User,
+    question: String,
+    governmentOffice: String,
+    files: [Buffer],
+    additionalQuestionAfterMembersReply: String,
+    // history 
+});
+
+const Suggestion = mongoose.model("Suggestion", SuggestionSchema);
+module.exports = Suggestion;
+
+
+
+const SuggestionStatusSchema = new mongoose.Schema({
+    status: String,
+    date: { type: Date, default: Date.now },
+});
+const SuggestionStatus = mongoose.model("SuggestionStatus", SuggestionStatusSchema);
+module.exports = SuggestionStatus;
