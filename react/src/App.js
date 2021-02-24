68a3f5422a1833b64be92fd59fcfe94f48b70db2
Ibrahim Lahham <abo.la7am.10@gmail.com>
2021-02-24 18:56:24 +0200

    app js update

--- react/src/App.js
+++ react/src/App.js
@@ -1,11 +1,7 @@
 import './App.css';
 import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
-import parliamentaryTool from './pages/parliamentaryTool/parliamentaryTool';
-import kenosKnesset from './pages/kenosKnesset/kenosKnesset';
-import normalquery from './pages/normalquery/normalquery';
-import oneMinuteSpeech from './pages/oneMinuteSpeech/oneMinuteSpeech';
-import trackingBoard from './pages/trackingBoard/trackingBoard';
+import parliamentaryTool from './pages/parliamentaryTool';
 
 
 import Login from './pages/loginRegisteration/loginRegisteration';
@@ -26,29 +22,11 @@ export default function App() {
       <div>
         <nav className='all-tabs'>
           <ul>
-<<<<<<< HEAD
-          <li className='active-nav'>
-              <Link to="/main">Login</Link>
-            </li>
-            <li className='non-active-nav'>
-              <Link to="/haverkneset">Haver</Link>
-            </li>
-            <li className='non-active-nav'>
-              <Link to="/resetpassword">reset</Link>
-            </li>
-            <li className='non-active-nav'>
-              <Link to="/forgetpassword">Forget</Link>
-=======
             <li className='active-nav'>
-              <Link to="/parliamentaryTool">parliamentaryTools</Link>
+              <Link to="/parliamentaryTool">Search</Link>
             </li>
             <li className='non-active-nav'>
-<<<<<<< Updated upstream
                <Link to="/">Form-create</Link>
->>>>>>> main
-=======
-               <Link to="/normalquery">normalquery</Link>
->>>>>>> Stashed changes
             </li>
           </ul>
         </nav>
@@ -59,7 +37,7 @@ export default function App() {
           <Route path="/parliamentaryTool">
             <FormCreate />
           </Route>
-          <Route path="/normalquery">
+          <Route path="/">
 
             <Login />
           </Route>
