cb5373b94d40212e8667acd8777b5ad598b6377f
Khalil Yasin <khaleal@hotmail.com>
2021-02-26 15:15:21 +0200

    added things

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,49 +1,43 @@
 import "./Header.css";
-
+import "./Breadcrumb";
+import Breadcrumb from "./Breadcrumb";
 function Header(props) {
-  let currentTabName = "כלים פרלמנטריים";
-  let userType = "לוח מעקב";
+  const breadcrumbList = [
+    { text: "לוח מעקב", url: "trackingBoard" },
+    { text: "כלים פרלמנטריים", url: "parliamentaryTool" },
+  ];
   return (
-    <div className="Header">
-      <div className="row">
-        <div id="logo" className="span4 clearfix">
-          <h1>
-            <a href="https://oknesset.org/">
-              <img
-                src="https://oknesset.org/static/img/oknesset-logo.png"
-                alt="oknesset-logo"
-              />
-              <span className="main-title">כנסת פתוחה</span>
-            </a>
-          </h1>
+    <div>
+      <div className="App-header">
+        <div className="Header">
+          <div className="row">
+            <div id="logo" className="span4 clearfix">
+              <h1>
+                <a href="https://oknesset.org/">
+                  <img
+                    src="https://oknesset.org/static/img/oknesset-logo.png"
+                    alt="oknesset-logo"
+                  />
+                  <span className="main-title">כנסת פתוחה</span>
+                </a>
+              </h1>
+            </div>
+          </div>
         </div>
+
+        <ul className="nav nav-pills">
+          <li id="nav-parties">
+            <a href="/members/index.html">ח'כים וסיעות</a>
+          </li>
+          <li id="nav-committees">
+            <a href="#">ועדות</a>
+          </li>
+          <li id="nav-committees">
+            <a href="#">כלים פרלמנטריים</a>
+          </li>
+        </ul>
       </div>
-
-      <ul className="nav nav-pills">
-        <li id="nav-parties">
-          <a href="/members/index.html">ח'כים וסיעות</a>
-        </li>
-        <li id="nav-committees">
-          <a href="#">ועדות</a>
-        </li>
-        <li id="nav-committees">
-          <a href="#">כלים פרלמנטריים</a>
-        </li>
-      </ul>
-      <section className="secondary-nav-bar">
-        <div className="path">
-          <a href="https://oknesset.org/"> דף הבית </a>/
-          <span className="current-tab-name"> {currentTabName} </span>
-        </div>
-
-        <div className="user-links">
-          <a className="disconnect" href="#">
-            {" "}
-            התנתק{" "}
-          </a>
-          |<a className="link"> {userType} </a>
-        </div>
-      </section>
+      <Breadcrumb breadcrumbList={breadcrumbList} userType={1} />
     </div>
   );
 }
