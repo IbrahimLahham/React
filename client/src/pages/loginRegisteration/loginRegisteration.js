86a6a4aea8eae707382e75b29bcb01de44037c5c
Jbareen Mohamad <jbareen.jeme@gmail.com>
2021-02-26 11:57:04 +0200

    fix

--- client/src/pages/loginRegisteration/loginRegisteration.js
+++ client/src/pages/loginRegisteration/loginRegisteration.js
@@ -1,43 +1,69 @@
-import React, { useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import './loginRegisteration.css'
 
 
 
-const LoginRegisteration = () => {
+function LoginRegisteration() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    
-    function handleLogin(e){
+
+    useEffect(() => {
+
+    }, []);
+
+    function handleLogin(e) {
         e.preventDefault();
         console.log(email);
         console.log(password);
-        fetch("/GET_LogIn", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({email,password}),
-          })
-            .then((r) => r.json())
-            .then((data) => {
-              console.log(data)
-            });
+        // fetch("/GET_LogIn", {
+        //     method: "POST",
+        //     headers: {
+        //       "Content-Type": "application/json",
+        //     },
+        //     body: JSON.stringify({email,password}),
+        //   })
+        //     .then((r) => r.json())
+        //     .then((data) => {
+        //       console.log(data)
+        //     });
     }
 
     return (
-        <div>
-            <div classname="user-login-div">
-                <h1 classname="title-bold-big">כניסה</h1>
-                <a classname="title-bold">דוא"ל:</a>
-                <input type="text" classname="input-field" onChange={(e) => {
-                    setEmail(e.target.value);
-                }}></input>
-                <a classname="title-bold">סיסמה:</a>
-                <input type="password" classname="input-field" onChange={(e) => {
-                    setPassword(e.target.value);
-                }}></input>
-                <button classname="user-button" onClick={handleLogin}>התחברות</button>
+        <div className="user-container">
+            <div className="user-login-div">
+                <h1 className="title-bold-big">כניסה</h1>
+                <div className="user-login-flex">
+                    <a className="title-bold">דוא"ל:</a>
+                    <input type="text" className="input-field" onChange={(e) => {
+                        setEmail(e.target.value);
+                    }}></input>
+                </div>
+                <div className="user-login-flex">
+                    <a className="title-bold">סיסמה:</a>
+                    <input type="password" className="input-field" onChange={(e) => {
+                        setPassword(e.target.value);
+                    }}></input>
+                </div>
+                <button className="user-button" onClick={handleLogin}>התחברות</button>
             </div>
+
+            <div className="user-login-div">
+                <h1 className="title-bold-big">כניסה</h1>
+                <div className="user-login-flex">
+                    <a className="title-bold">דוא"ל:</a>
+                    <input type="text" className="input-field" onChange={(e) => {
+                        setEmail(e.target.value);
+                    }}></input>
+                </div>
+                <div className="user-login-flex">
+                    <a className="title-bold">סיסמה:</a>
+                    <input type="password" className="input-field" onChange={(e) => {
+                        setPassword(e.target.value);
+                    }}></input>
+                </div>
+                <button className="user-button" onClick={handleLogin}>התחברות</button>
+            </div>
+
         </div>
     )
 }
