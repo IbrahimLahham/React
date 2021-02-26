5b83c3cc720ff2a6d280465e8f8238bc0e782a58
Tal <tal.yaron@gmail.com>
2021-02-26 09:36:14 +0200

    my amazing improvment to the code

--- schema/Tool.js
+++ schema/Tool.js
@@ -1,13 +1,7 @@
 const mongoose = require("mongoose");
 
 
-const ToolSchema = new mongoose.Schema({
-    type: String,
-    title: String,
-    subTitle: String,
-    term: String,
-    language: String
-});
+
 
 
 const Tool = mongoose.model("Tool", ToolSchema);
