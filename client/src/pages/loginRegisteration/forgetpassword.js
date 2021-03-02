6114caa5fb9696361b9d9c815fa90fcc0721052d
يارا كبها <yara.kbha4@gmail.com>
2021-03-02 21:50:03 +0200

    login&sigup fetch

--- client/src/pages/loginRegisteration/forgetpassword.js
+++ client/src/pages/loginRegisteration/forgetpassword.js
@@ -6,11 +6,27 @@ import React, { useState } from 'react';
 function Forgetpassword() {
 
     const [email, setEmail] = useState("");
-
+    const[success,setSuccess]=useState("");
     function handleForgot(e){
         e.preventDefault();
         console.log(email);
-        alert(`email: ${email}`);
+        fetch('/user/ForgetPassword', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email })
+        }).then(r => r.json())
+            .then(data => {
+                console.log(data);
+                if (data.email === true) {
+                    setSuccess("check out your email to reset password")
+                }
+                else{
+                    setSuccess("email not exist please sign up!")
+                }
+
+            })
     }
 
     return (
@@ -24,6 +40,7 @@ function Forgetpassword() {
                     }}></input>
                 </div>
                 <button className="user-button" onClick={handleForgot}>שחזור</button>
+                <label className="message">{success}</label>
             </div>
         </div>
     )
--- client/src/pages/loginRegisteration/loginRegisteration.js
+++ client/src/pages/loginRegisteration/loginRegisteration.js
@@ -3,7 +3,7 @@ import './loginRegisteration.css'
 import Forgetpassword from './forgetpassword';
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
-
+import { useHistory } from 'react-router-dom';
 
 import {
     BrowserRouter as Router,
@@ -14,7 +14,7 @@ import {
 
 
 function LoginRegisteration() {
-
+    const history = useHistory();
     const [loginEmail, setLoginEmail] = useState("");
     const [loginPassword, setLoginPassword] = useState("");
     const [email, setEmail] = useState("");
@@ -22,22 +22,59 @@ function LoginRegisteration() {
     const [lastName, setLastName] = useState("");
     const [telephon, setTelephon] = useState("");
     const [organization, setOrganization] = useState("");
-
-    const [loginAll, setLoginAll] = useState([]);
-    const [registerAll, setRegisterAll] = useState([]);
+    const [error,setError]=useState("");
+    const [registermessage,setRegisterMessage]=useState("");
 
     useEffect(() => {
 
     }, []);
 
     function handleLogin(e) {
-        e.preventDefault();
-        console.log({ email: loginEmail, password: loginPassword })
+        fetch('/user/Login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email:loginEmail, password:loginPassword })
+        }).then(r => r.json())
+            .then(data => {
+                console.log(data);
+                //if login true - redirect to forms creation page;
+                if (data.login === true) {
+                    if(data.type === "citizen")
+                    history.push('/parliamentaryTool')
+                    else{
+                        history.push('/haverKnesset')
+                    }
+                }
+                else{
+                    setError("email or password not correct!!")
+                }
+
+            })
     }
 
     function handleRegister(e) {
         e.preventDefault();
         console.log({ firstName: firstName, lastName: lastName, email: email, organization: organization, telephon: telephon })
+        fetch('/user/Registration', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ firstName:firstName, lastName:lastName,email:email ,company:organization,phone:telephon})
+        }).then(r => r.json())
+            .then(data => {
+                console.log(data);
+                //if login true - redirect to forms creation page;
+                if (data.rgisteer === true) {
+                    setRegisterMessage("just one step!check your mail to set a paasword for you!")
+                }
+                else{
+                    setRegisterMessage("the email is exist!")
+                }
+
+            })
     }
 
     return (
@@ -62,6 +99,7 @@ function LoginRegisteration() {
                     <Link id="forgot-pass" to="/forgetpassword">שכחתי סיסמה</Link>
 
                     <button className="user-button" onClick={handleLogin}>התחברות</button>
+                    <label className="message">{error}</label>
                 </div>
 
                 <div className="user-login-div">
@@ -92,12 +130,13 @@ function LoginRegisteration() {
                     </div>
                     <div className="user-login-flex">
                         <label className="title-bold">טלפון:</label>
-                        <input type="number" className="input-field" onChange={(e) => {
+                        <input type="text" className="input-field" onChange={(e) => {
                             setTelephon(e.target.value);
                         }}></input>
                     </div>
                     <label id="required" className="paragraph-regular">*חובה למלא פרטים המסומנים בכוכבית</label>
                     <button className="user-button" onClick={handleRegister}>הרשמה</button>
+                    <label className="message">{registermessage}</label>
                 </div>
 
             </div>
