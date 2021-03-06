9d3657e7c4fd2d7fb2fa8da2eff5ea9d7cfadc72
Khalil Yasin <khaleal@hotmail.com>
2021-03-06 19:00:03 +0200

    added trackingboard + regular query + one mintue speech pages

---
+++ client/src/components/OneMinuteForm.js
@@ -0,0 +1,139 @@
+import React from "react";
+import "./OneMinuteForm.css";
+import Subject from "./Subject";
+import at from "./Images/at.PNG";
+import { Multiselect } from "multiselect-react-dropdown";
+
+import AttachIcon from "./Images/attach.png";
+import AttachmentsIcon from "./Images/attacments-icon.png";
+import PeopleIcon from "./Images/people.png";
+
+function OneMinuteForm() {
+  const options = [
+    { name: "גפני משה", id: 1 },
+    { name: "גרמל יעל", id: 2 },
+    { name: "דיין עוזי", id: 1 },
+    { name: "דיכנטר אבי", id: 2 },
+    { name: "Srigar", id: 1 },
+    { name: "Sam", id: 2 },
+    { name: "Srigar", id: 1 },
+    { name: "Sam", id: 2 },
+    { name: "Srigar", id: 1 },
+    { name: "Sam", id: 2 },
+    { name: "Srigar", id: 1 },
+    { name: "Sam", id: 2 },
+    { name: "Srigar", id: 1 },
+    { name: "Sam", id: 2 },
+    { name: "Srigar", id: 1 },
+    { name: "Sam", id: 2 },
+    { name: "Srigar", id: 1 },
+    { name: "Sam", id: 2 },
+  ];
+  function dummy() {
+    console.log("hehe");
+  }
+
+  function onSelect(selectedList, selectedItem) {
+    console.log("select invoked: ", selectedList);
+  }
+
+  function handleForm(e) {
+    e.preventDefault();
+    console.log(e.target.subject.value);
+  }
+  return (
+    <div className="recomnde">
+      <Subject Icon={at} text="הגש הצעה" />
+      <form id="myform" onSubmit={handleForm}>
+        <div className="recomend__info">
+          <div className="reomnde__SecindHalf">
+            <h4>נושא הצעה לסדר:</h4>
+            <input type="text" name="subject" />
+            <h4>דברי הסבר:</h4>
+            <textarea name="" id="" cols="47" rows="12"></textarea>
+          </div>
+          <div className="reomnde__FirstHalf">
+            <h4>חכ"ים רלוונטיים: </h4>
+            {/* <select className="select">
+            <option>גפני משה</option>
+            <option>גרמל יעל</option>
+            <option>דיין עוזי</option>
+            <option>דיכנטר אבי</option>
+          </select> */}
+            <Multiselect
+              style={{
+                multiselectContainer: {
+                  width: "300px",
+                },
+                inputField: {
+                  margin: "5px",
+                },
+                optionContainer: {
+                  // To change css for option container
+                  width: "300px",
+                  "text-align": "right",
+                },
+                searchBox: {
+                  // To change search box element look
+                  "text-align": "right",
+                  color: "#000",
+                },
+                option: {
+                  // To change css for dropdown options
+
+                  "font-size": "12px",
+                },
+                "option:hover": {
+                  // To change css for dropdown options
+                  "background-color": "grey",
+                  "font-size": "12px",
+                },
+              }}
+              options={options} // Options to display in the dropdown
+              selectedValues={dummy} // Preselected value to persist in dropdown
+              onSelect={onSelect} // Function will trigger on select event
+              onRemove={dummy} // Function will trigger on remove event
+              displayValue="name" // Property name to display in the dropdown options
+              placeholder="בחר חבר כנסת"
+            />
+            <br />
+            <div class="attach-title-div">
+              <img src={AttachIcon}></img>
+              <h4>קבצים ומסמכים תומכים </h4>
+            </div>
+            <div className="attachments-div">
+              <div className="attachments-list">
+                <div class="attachment">
+                  <img src={AttachmentsIcon}></img>
+                  <p>פרוטוקול אסיפה כללית 9.9.13</p>
+                  <img src={PeopleIcon}></img>
+                </div>
+                <div class="attachment">
+                  <img src={AttachmentsIcon}></img>
+                  <p>פרוטוקול אסיפה כללית 9.9.13</p>
+                  <img src={PeopleIcon}></img>
+                </div>
+                <div class="attachment">
+                  <img src={AttachmentsIcon}></img>
+                  <p>פרוטוקול אסיפה כללית 9.9.13</p>
+                  <img src={PeopleIcon}></img>
+                </div>
+              </div>
+              <div className="attach-button-div">
+                <button className="btn" type="submit">
+                  הוסף קובץ
+                </button>
+              </div>
+            </div>
+
+            <button className="btn" type="submit">
+              שלח
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default OneMinuteForm;
--- client/src/pages/trackingBoard/trackingBoard.js
+++ client/src/pages/trackingBoard/trackingBoard.js
@@ -1,18 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import "./trackingBoard.css";
 
 //import './normalquery.css'
 
-
 const trackingBoard = () => {
-
   // const [book, setbook] = useState([]);
-  
 
-    return (
-        <div>
-            <h1>trackingBoard page</h1>
+  return (
+    <div>
+      <div className="container">
+        <div className="board-item-container">
+          <div className="board-item">
+            <p>21.11.20 נאום בן דקה</p>
+            <h1>סגירת מרכולים בבני ברק</h1>
+            <p>תאריך הגשה: </p>
+            <p>אומץ על ידי: </p>
+            <p>הוקרא במליאה: </p>
+            <h1>תוכן:</h1>
+            <p>נודע לי כי...</p>
+          </div>
         </div>
-    )
-}
+        <div className="board-item-container">
+          <div className="board-item">
+            <p>21.11.20 נאום בן דקה</p>
+            <h1>סגירת מרכולים בבני ברק</h1>
+            <p>תאריך הגשה: </p>
+            <p>אומץ על ידי: </p>
+            <p>הוקרא במליאה: </p>
+            <h1>תוכן:</h1>
+            <p>נודע לי כי...</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default trackingBoard
+export default trackingBoard;
