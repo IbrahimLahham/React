1d54c311f8cdf17cc10bd910445f12a3dc693516
Jbareen Mohamad <jbareen.jeme@gmail.com>
2021-03-10 00:18:54 +0200

    fix disconnect and remove cookie

--- client/src/pages/loginRegisteration/loginRegisteration.js
+++ client/src/pages/loginRegisteration/loginRegisteration.js
@@ -1,8 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import './loginRegisteration.css'
-import Forgetpassword from './forgetpassword';
-import Header from '../../components/Header'
-import Footer from '../../components/Footer'
 import { useHistory } from 'react-router-dom';
 
 import {
