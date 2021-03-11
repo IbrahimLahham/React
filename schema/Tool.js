a7d425d2845fb949fb0dcf738cd9508d181e8452
Hosen <hosen.agbaria@hotmail.com>
2021-03-11 21:23:12 +0200

    clean up

--- schema/Tool.js
+++ schema/Tool.js
@@ -1,16 +1,13 @@
 const mongoose = require("mongoose");
 
-
 const ToolSchema = new mongoose.Schema({
-    type: String,
-    title: String,
-    subTitle: String,
-    term: String,
-    language: String,
-    redirectTo:String
+  type: String,
+  title: String,
+  subTitle: String,
+  term: String,
+  language: String,
+  redirectTo: String,
 });
-
-
 
 const Tool = mongoose.model("Tool", ToolSchema);
 module.exports = Tool;
