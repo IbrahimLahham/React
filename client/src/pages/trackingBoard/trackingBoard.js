00b7420824b91b404132cd04891256b8d2fbac28
Khalil Yasin <khaleal@hotmail.com>
2021-03-18 17:02:28 +0200

    tracking board connect to api

--- client/src/pages/trackingBoard/trackingBoard.js
+++ client/src/pages/trackingBoard/trackingBoard.js
@@ -4,23 +4,42 @@ import "./trackingBoard.css";
 //import './normalquery.css'
 
 const TrackingBoard = () => {
-  // const [book, setbook] = useState([]);
- // const [data, setData] = useState([]);
+  const [suggestions, setSuggestions] = useState([]);
 
   useEffect(() => {
-      fetch("/suggestion/byUserSuggest")
+    fetch("/suggestion/byUserSuggest")
       .then((r) => r.json())
       .then((data) => {
-        console.log(data);
-        alert("am in");
+        console.log("Suggestions data: ", data);
+        if (data.ok === true) {
+          setSuggestions(data.suggestions);
+        }
+        //alert("am in");
       });
-
   }, []);
 
   return (
     <div>
       <div className="container">
-        <div className="board-item-container">
+        {suggestions &&
+          suggestions.map((suggestion, index) => {
+            return (
+              <div className="board-item-container">
+                <div className="board-item">
+                  <p>
+                    {suggestion.date} {suggestion.toolType.title}
+                  </p>
+                  <h1>{suggestion.subject}</h1>
+                  <p>תאריך הגשה: {suggestion.date}</p>
+                  <p>אומץ על ידי:</p>
+                  <p>הוקרא במליאה: {console.log("Suggestion: ", suggestion)}</p>
+                  <h1>תוכן:</h1>
+                  <p>{suggestion.description}</p>
+                </div>
+              </div>
+            );
+          })}
+        {/* <div className="board-item-container">
           <div className="board-item">
             <p>21.11.20 נאום בן דקה</p>
             <h1>סגירת מרכולים בבני ברק</h1>
@@ -41,7 +60,7 @@ const TrackingBoard = () => {
             <h1>תוכן:</h1>
             <p>נודע לי כי...</p>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
