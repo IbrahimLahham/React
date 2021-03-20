5fc009a99bda4e955a9a74a61d9943c0b4c026bb
rema <remawail06@gmail.com>
2021-03-21 00:07:19 +0200

    update

--- client/src/App.js
+++ client/src/App.js
@@ -26,7 +26,7 @@ export default function App() {
   const [connected, setConnected] = useState(false);
   const [user, setUser] = useState({});
 
-  const DEBUG = true;
+  const DEBUG = false;
 
   useEffect(() => {
     fetch("/user/checkConnection", {
